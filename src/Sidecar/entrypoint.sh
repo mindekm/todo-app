8cdd5337823e4ad6db5ab6fa65b7298762300b98
Milan Mindek <milan.mindek@gmail.com>
2023-10-01 15:48:03 +0200

    add envoy sidecar

---
+++ src/Sidecar/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sed -e "s/\${SERVICE_NAME}/${SERVICE_NAME}/" \
+    -e "s/\${SERVICE_PORT}/${SERVICE_PORT}/" \
+    -e "s/\${LISTENER_PORT}/${LISTENER_PORT}/" \
+    /config/envoy.yaml > /etc/envoy.yaml
+
+usr/local/bin/envoy -c /etc/envoy.yaml -l ${DEBUG_LEVEL}
